64701a243fbe4b9515921ea0dcaedd3770ece48d
shahzad50 <95250613+shahzad50@users.noreply.github.com>
2024-12-29 17:27:25 +0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,36 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "66iwILJSNf6":
+        Script1();
+        break;
+      case "69mlYz1NV6A":
+        Script2();
+        break;
+      case "5e2figqruje":
+        Script3();
+        break;
+      case "5eHI52eBF40":
+        Script4();
+        break;
+      case "5t1LxX1qTwT":
+        Script5();
+        break;
+      case "6JL4mMSjmfo":
+        Script6();
+        break;
+      case "5tYzrHORz8J":
+        Script7();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
